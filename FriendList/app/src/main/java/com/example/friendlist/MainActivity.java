1eee51de0a76caa2e5f190aaa0161201ff9ec355
Asim <asimafzal369@gmail.com>
2022-05-25 20:11:36 +0500

    Getting values from user and add in List View

--- FriendList/app/src/main/java/com/example/friendlist/MainActivity.java
+++ FriendList/app/src/main/java/com/example/friendlist/MainActivity.java
@@ -3,19 +3,25 @@ package com.example.friendlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
     ArrayList<String> al = new ArrayList<>();
+
     ListView lv ;
+    EditText n ;
+    ImageView plus;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,10 +32,22 @@ public class MainActivity extends AppCompatActivity {
         al.add("Mahad");
         al.add("Faizan");
 
-        ArrayAdapter<String> ad = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,al);
-
         lv = findViewById(R.id.frndlst);
+        n = findViewById(R.id.enter_name);
+        plus = findViewById(R.id.add);
+
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,al);
         lv.setAdapter(ad);
+
+        plus.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                al.add(n.getText().toString());
+                ad.notifyDataSetChanged();
+                n.setText("");
+            }
+        });
 
 
     }
